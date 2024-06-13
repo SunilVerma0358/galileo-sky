d0a954ca08cf0eb2c6565985c0422e028c1c572a
SunilVerma0358 <sunilverma93060@gmail.com>
2024-06-13 11:33:42 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      container: { padding: "12px", center: true },
+      fontFamily: {
+        poppins: ["Poppins, sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        white: "#F7F7F7",
+        "white-cream-btn": "#FEFEFE",
+        "black-dark": "#010101",
+        "dark-gray": "#4B4B4B",
+        "red-dark": "#96151D",
+        "red-light": "#EB484C",
+        "hero-section-gray": "#E6E6E6",
+        "input-gray": "#DFDEDE",
+        "gray-dark-input": "#444343",
+        "border-color-gray": "#D2D2D2",
+        "nav-bg": "#FEFEFE2B",
       },
     },
   },
